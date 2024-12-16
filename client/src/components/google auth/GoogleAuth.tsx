999b15c4bb8df9857d775056094783738f91c1c3
miladBann <miladbannourah@outlook.com>
2024-12-16 02:04:43 +0200

    fixed the authentication component to give an OAuth token that can be used to access and modify user data on youtube

--- client/src/components/google auth/GoogleAuth.tsx
+++ client/src/components/google auth/GoogleAuth.tsx
@@ -1,40 +1,37 @@
 import React, { useState } from "react";
-import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
-//import styles from "./google-auth.module.css";
+import { useGoogleLogin, googleLogout } from "@react-oauth/google";
 
 const GoogleAuth: React.FC = () => {
-  const [user, setUser] = useState<any>(null);
+  const [userToken, setUserToken] = useState<string | null>(null);
 
-  const handleLoginSuccess = (credentialResponse: any) => {
-    console.log("Login Success:", credentialResponse);
-    setUser(credentialResponse);
-    localStorage.setItem("credintial", credentialResponse.credential);
-  };
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      console.log("Access Token:", tokenResponse.access_token);
+      setUserToken(tokenResponse.access_token);
+      localStorage.setItem("accessToken", tokenResponse.access_token);
+    },
+    scope: "https://www.googleapis.com/auth/youtube.readonly",
+    flow: "implicit",
+  });
 
   const handleLogout = () => {
     googleLogout();
-    setUser(null);
+    setUserToken(null);
+    localStorage.removeItem("accessToken");
     console.log("Logged out successfully");
   };
 
   return (
-    <GoogleOAuthProvider clientId="704853235569-heorv705ni3to0l35d8sjvdk7o44jbpl.apps.googleusercontent.com">
-      <div>
-        {!user ? (
-          <GoogleLogin
-            onSuccess={handleLoginSuccess}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-          />
-        ) : (
-          <div>
-            <p>Welcome, user is logged in!</p>
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-        )}
-      </div>
-    </GoogleOAuthProvider>
+    <div>
+      {!userToken ? (
+        <button onClick={() => login()}>Login with Google</button>
+      ) : (
+        <div>
+          <p>Welcome! Access Token Retrieved.</p>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      )}
+    </div>
   );
 };
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="704853235569-heorv705ni3to0l35d8sjvdk7o44jbpl.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
